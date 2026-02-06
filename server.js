ea41588de3d699fb90a43c6b9d9a57419d885020
Nikita Kapaev <nikita.kapaev@gmail.com>
2026-02-06 13:33:56 +0200

    Fix parser port

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.post('/get-metadata', async (req, res) => {
     }
 });
 
-app.listen(4000, () => console.log('Server running on http://localhost:4000'));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
